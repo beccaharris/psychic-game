72053f00063dd0aee33b4e79c6c703db7f5b6eab
Becca Harris <bharris@providigm.com>
2018-02-13 22:56:30 -0700

    get it working!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,10 @@ var guessesMade = []
 
 // Resets the guessesLeft, guessesMade, and sets a new ComputerGuess // 
 function resetGuess() {
-  guessesMade.length = 0;
-  guessesLeft = 9;
   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  guessesMade.splice(0)
+  guessesLeft = 9;
+  
 }
 
 document.onkeyup = function(event) {
@@ -20,44 +21,31 @@ document.onkeyup = function(event) {
     console.log(guessesMade)
   } else if (guessesMade.indexOf(playerGuess) > -1) {
     alert("You already guessed me! Try another letter")
-    } else if (computerChoices.indexOf(playerGuess) === -1) {
-      alert("You picked an invalid character! Pick a letter")
+  } else if (computerChoices.indexOf(playerGuess) === -1) {
+    alert("You picked an invalid character! Pick a letter")
   } 
+
+  if (playerGuess === computerGuess) {
+    alert("You won!");
+    wins++;
+    resetGuess()
+  } if (playerGuess !== computerGuess) {
+    guessesLeft--
+  } if (guessesLeft === 0) {
+    alert("You lost :( - better luck next time! Click OK to reset");
+    losses++;
+    resetGuess()
+  }
+
+  var html = 
+  '<h1>Psychic Game</h1>' +
+  '<h2>Press any letter key to start!</h2>'+
+  '<p>Wins: ' + wins + '</p>' +
+  '<p>Losses: ' + losses + '</p>' +
+  '<p>Guesses Remaining: ' + guessesLeft + '</p>' +
+  '<p>Your Previous Guesses: ' + guessesMade + '</p>'
+
+  document.querySelector('#psychic-game').innerHTML = html;
 }
-
-
-  
-  // Alert the player if they are guessing something other than a-z // 
-  //if (guessesMade.indexOf(playerGuess) >= 0 && computerGuess.indexOf(playerGuess) === -1) {
-  //  alert("try again!")
-  //}
-  // If the guess 
-  //if (guessesMade.indexOf(playerGuess) === -1 && computerGuess.indexOf(playerGuess) >= 0) {
-  //  guessesMade.push(playerGuess);
-  //  console.log("New guessesMade array is: " + guessesMade)
-  //} 
-//
-  //if (playerGuess === computerGuess) {
-  //  alert("You won!");
-  //  wins++;
-  //  resetGuess()
-  ////} else if (playerGuess !== computerGuess) {
-  ////  guessesLeft--;
-  //} if (guessesLeft === 0) {
-  //  alert("You lost! None of your guesses were correct");
-  //  losses++;
-  //  reset()
-  //}
-  //console.log(computerGuess + " Computer guess")
-  //var html = 
-  //'<h1>Psychic Game</h1>' +
-  //'<h2>Press any letter key to start!</h2>'+
-  //'<p>Wins: ' + wins + '</p>' +
-  //'<p>Losses: ' + losses + '</p>' +
-  //'<p>Guesses Remaining: ' + guessesLeft + '</p>' +
-  //'<p>Your Previous Guesses: ' + guessesMade + '</p>'
-//
-  //document.querySelector('#psychic-game').innerHTML = html;
-
 
 
