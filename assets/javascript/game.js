0cf950a122b09ab0f11778589fde99e66a447c8c
Becca Harris <bharris@providigm.com>
2018-02-14 08:18:17 -0700

    little cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,16 +9,15 @@ var guessesMade = []
 // Resets the guessesLeft, guessesMade, and sets a new ComputerGuess // 
 function resetGuess() {
   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  guessesMade.splice(0)
+  guessesMade.splice(0, guessesMade.length)
   guessesLeft = 9;
-  
 }
 
 document.onkeyup = function(event) {
   var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  
   if ((guessesMade.indexOf(playerGuess) === -1) && (computerChoices.indexOf(playerGuess) !== -1)) {
     guessesMade.push(playerGuess)
-    console.log(guessesMade)
   } else if (guessesMade.indexOf(playerGuess) > -1) {
     alert("You already guessed me! Try another letter")
   } else if (computerChoices.indexOf(playerGuess) === -1) {
@@ -38,14 +37,12 @@ document.onkeyup = function(event) {
   }
 
   var html = 
-  '<h1>Psychic Game</h1>' +
-  '<h2>Press any letter key to start!</h2>'+
   '<p>Wins: ' + wins + '</p>' +
   '<p>Losses: ' + losses + '</p>' +
   '<p>Guesses Remaining: ' + guessesLeft + '</p>' +
   '<p>Your Previous Guesses: ' + guessesMade + '</p>'
 
-  document.querySelector('#psychic-game').innerHTML = html;
+  document.querySelector('#game-content').innerHTML = html;
 }
 
 
