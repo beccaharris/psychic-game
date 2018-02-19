5d09e0d360534aa0df6b89efe95bb09d92c563bb
Becca Harris <beccalynharris@gmail.com>
2018-02-18 19:13:00 -0700

    add sounds and a little styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,12 @@ var losses = 0;
 var guessesLeft = 9;
 var guessesMade = []
 
+// Audio files
+var buzzerSound = new Audio('assets/audio/buzzer_x.wav');
+var cheeringSound = new Audio('assets/audio/cheering.wav');
+var cuckooClockSound = new Audio('assets/audio/cuckoo_clock1_x.wav');
+var fartSound = new Audio('assets/audio/quick_fart_x.wav');
+
 // Resets the guessesLeft, guessesMade, and sets a new ComputerGuess // 
 function resetGuess() {
   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -18,19 +24,23 @@ document.onkeyup = function(event) {
   if ((guessesMade.indexOf(playerGuess) === -1) && (computerChoices.indexOf(playerGuess) !== -1)) {
     guessesMade.push(playerGuess);
     if (playerGuess === computerGuess) {
+      cheeringSound.play();
       alert("You won!");
       wins++;
       resetGuess()
     } else if (playerGuess !== computerGuess) {
       guessesLeft--
     } if (guessesLeft === 0) {
+      fartSound.play();
       alert("You lost! The computer guessed the letter: " + computerGuess.toUpperCase() + ". Better luck next time! Click OK to reset");
       losses++;
       resetGuess()
     }
   } else if (guessesMade.indexOf(playerGuess) > -1) {
-      alert("You already guessed me! Try another letter")
+    cuckooClockSound.play(); 
+    alert("You already guessed me! Try another letter");
   } else if (computerChoices.indexOf(playerGuess) === -1) {
+      buzzerSound.play();
       alert("You picked an invalid character! Pick a letter")
   } 
 
